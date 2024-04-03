e3b2650b52b4943c580b9df6ed3258bf3dc0ea5a
Valeria Zaborovskaia <VViZaborovskaya@sberbank.ru>
2024-04-03 11:49:05 +0300

    - audio segmentation pipeline improved - readme improved

--- audio_segmentation.py
+++ audio_segmentation.py
@@ -5,6 +5,9 @@ from IPython.display import Audio, display
 
 import ruptures as rpt
 
+# Choose the number of changes (elbow heuristic)
+n_bkps_max = 20  # K_max
+
 
 def fig_ax(figsize=(15, 5), dpi=150):
     """Return a (matplotlib) figure and ax objects with given size."""
@@ -12,12 +15,17 @@ def fig_ax(figsize=(15, 5), dpi=150):
 
 
 def get_sum_of_cost(algo, n_bkps):
-        """Return the sum of costs for the change points `bkps`"""
-        bkps = algo.predict(n_bkps=n_bkps)
-        return algo.cost.sum_of_costs(bkps)
+    """Return the sum of costs for the change points `bkps`"""
+    bkps = algo.predict(n_bkps=n_bkps)
+    return algo.cost.sum_of_costs(bkps)
 
 
 def plot_decision_graph(algo, array_of_n_bkps):
+    """
+    Строит график, по которому принимается решение о количестве changepoint-ов
+    @param algo -- алгоритм
+    @param array_of_n_bkps) --
+    """
     fig, ax = fig_ax((7, 4))
     ax.plot(
         array_of_n_bkps,
@@ -30,8 +38,8 @@ def plot_decision_graph(algo, array_of_n_bkps):
     ax.set_title("Sum of costs")
     ax.grid(axis="x")
     ax.set_xlim(0, n_bkps_max + 1)
-    fig.savefig('foo.png')
-    print("Pic saved to 'foo.png'")
+    fig.savefig('data/decision.png')
+    print("Pic saved to data/decision.png")
 
 
 def compute_tempogram(sampling_rate, oenv, hop_length_tempo):
@@ -51,20 +59,18 @@ def compute_tempogram(sampling_rate, oenv, hop_length_tempo):
         x_axis="s",
         y_axis="tempo",
     )
-    fig.savefig("tempo.png")
-    print("Tempogram saved")
+    fig.savefig("data/tempo.png")
+    print("Tempogram saved to " + "data/tempo.png")
     return tempogram
 
 
-if __name__ == "__main__":
-    duration = 213  # in seconds
-    #print(librosa.ex("nutcracker"))
-    filename = '/Users/21415968/Desktop/Nirvana-Smells-like-teen-spirit.ogg'
-    signal, sampling_rate = librosa.load(filename, duration=duration)
-    #signal, sampling_rate = librosa.load(librosa.ex("nutcracker"), duration=duration)
+def segmentation(filename, duration):
+    """
+    @param filename -- абсолютный путь до аудио файла (.ogg)
+    @param duration -- продолжительность отрезка аудио в секундах
+    """
 
-    # listen to the music
-    # display(Audio(data=signal, rate=sampling_rate))
+    signal, sampling_rate = librosa.load(filename, duration=duration)
 
     # look at the envelope
     fig, ax = fig_ax()
@@ -72,6 +78,9 @@ if __name__ == "__main__":
     ax.set_xlim(0, signal.size / sampling_rate)
     ax.set_xlabel("Time (s)")
     _ = ax.set(title="Sound envelope")
+
+    fig.savefig("data/sound-envelope.png")
+    print("Sound envelope saved to " + "data/sound-envelope.png")
 
     # Compute the onset strength
     hop_length_tempo = 256
@@ -83,8 +92,6 @@ if __name__ == "__main__":
     # Choose detection method
     algo = rpt.KernelCPD(kernel="linear").fit(tempogram.T)
 
-    # Choose the number of changes (elbow heuristic)
-    n_bkps_max = 20  # K_max
     # Start by computing the segmentation with most changes.
     # After start, all segmentations with 1, 2,..., K_max-1 changes are also available for free.
     _ = algo.predict(n_bkps_max)
@@ -94,9 +101,12 @@ if __name__ == "__main__":
     plot_decision_graph(algo, array_of_n_bkps)
 
     # Visually we choose n_bkps=5 (highlighted in red on the elbow plot)
+    # TODO придумать как тут раскумекать с количеством changepoint-ов
+    #  например вызывать методы в два этапа или типо того
+    # print("Please check out decision.png and choose number of changepoints")
+    # n_bkps = int(input())
     n_bkps = 6
     _ = ax.scatter([5], [get_sum_of_cost(algo=algo, n_bkps=5)], color="r", s=100)
-
 
     # Segmentation
     bkps = algo.predict(n_bkps=n_bkps)
@@ -105,24 +115,32 @@ if __name__ == "__main__":
 
     # Displaying results
     fig, ax = fig_ax()
-    _ = librosa.display.specshow(tempogram,ax=ax,x_axis="s",y_axis="tempo",hop_length=hop_length_tempo,sr=sampling_rate,)
-    fig.savefig("result.png")
-    print("Result file saved")
+    _ = librosa.display.specshow(tempogram, ax=ax, x_axis="s", y_axis="tempo", hop_length=hop_length_tempo,
+                                 sr=sampling_rate, )
+    fig.savefig("data/result.png")
+    print("Result file saved to " + "data/result.png")
 
     for b in bkps_times[:-1]:
         ax.axvline(b, ls="--", color="white", lw=4)
-
 
     # Compute change points corresponding indexes in original signal
     bkps_time_indexes = (sampling_rate * bkps_times).astype(int).tolist()
 
     for segment_number, (start, end) in enumerate(
-        rpt.utils.pairwise([0] + bkps_time_indexes), start=1
+            rpt.utils.pairwise([0] + bkps_time_indexes), start=1
     ):
         segment = signal[start:end]
         # ВРЕМЯ В СЕКУНДАХ
-        print("Start", round(start/sampling_rate, 3), "end", round(end/sampling_rate, 3))
-        #print(f"Segment n°{segment_number} (duration: {segment.size/sampling_rate:.2f} s)")
-        #display(Audio(data=segment, rate=sampling_rate))
+        print("Start", round(start / sampling_rate, 3), "end", round(end / sampling_rate, 3))
+        # print(f"Segment n°{segment_number} (duration: {segment.size/sampling_rate:.2f} s)")
+        # display(Audio(data=segment, rate=sampling_rate))
 
-    print(np.array(bkps_time_indexes)/sampling_rate)
+    result = np.array(bkps_time_indexes) / sampling_rate
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    duration = 60  # in seconds
+    filename = '/Users/21415968/Desktop/Nirvana-Smells-like-teen-spirit.ogg'
+    result = segmentation(filename, duration)
