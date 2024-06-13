1325cafb3d944c000b33bb8d7a8fd180403b8c53
Valeria Zaborovskaia <VViZaborovskaya@sberbank.ru>
2024-06-13 15:42:40 +0300

    added get boundaries method with kernel inside

--- SMSA/audio_domain/ruptures/pelt.py
+++ SMSA/audio_domain/ruptures/pelt.py
@@ -1,11 +1,16 @@
 
 import ruptures as rpt
 import logging as log
+import numpy as np
 
 from SMSA.audio_domain.AudioPeltInterface import AudioPeltInterface
 from SMSA.audio_domain.ruptures.utils.tempogram import bkps2sec
 from SMSA.audio_domain.ruptures.utils.tempogram import tempo
 from config import jump, penalty
+
+
+import ruptures as rpt  # our package
+
 
 
 class pelt(AudioPeltInterface):
@@ -14,9 +19,14 @@ class pelt(AudioPeltInterface):
 
     def fit(self, filename: str, signal) -> rpt.Pelt:
         log.info(f"Using pelt algo to compute segmentation")
-        print(f"Current min_size {int(signal.T.shape[0] / 8)}")
-        print(f"Current jump {int(signal.T.shape[0] / 800)}")
+
         tempogram = signal.T
+        tempo_len: int = tempogram.shape[0]
+
+        print(f"Current min_size {int(tempo_len / 15)}")
+        print(f"Current jump {int(tempo_len / 1500)}")
+        print(tempogram.shape)
+
         algo: rpt.Pelt = rpt.Pelt(
             model="rbf",
             min_size=int(tempogram.shape[0] / 15),
@@ -35,6 +45,13 @@ class pelt(AudioPeltInterface):
 
 
 if __name__ == "__main__":
-    p = pelt()
-    filename = "/Users/21415968/Desktop/diploma/symbolic-music-structure-analysis/data/RussianPop/batarei_nervi/batarei_nervi.mp3"
-    print(p.predict(filename))
+    # p = pelt()
+    # filename = "/Users/21415968/Desktop/diploma/symbolic-music-structure-analysis/data/BPS_FH_Dataset/15/15.mp3"
+    # print(p.predict(filename))
+
+    # generate signal
+    n_samples, dim, sigma = 500, 3, 3
+    n_bkps = 6  # number of breakpoints
+    signal, bkps = rpt.pw_constant(n_samples, dim, n_bkps, noise_std=sigma)
+    print(signal.reshape(-1,1).shape[0])
+    print(np.zeros((80362, 384)).reshape(-1, 1).shape[0] / signal.reshape(-1,1).shape[0])
--- SMSA/symbolic_domain/SymbolicPelt.py
+++ SMSA/symbolic_domain/SymbolicPelt.py
@@ -1,11 +1,22 @@
 import os
 import logging as log
-import warnings
-from pathlib import Path
+
 from musicaiz.features import StructurePrediction
-import numpy as np
+from musicaiz.loaders import Musa
+
 from SMSA.Segmenter import Segmenter
-from SMSA.Dataparser import construct_filename_with_your_extension
+import warnings
+import ruptures as rpt
+import numpy as np
+import networkx as nx
+
+from pathlib import Path
+
+from musicaiz.features import (
+    get_novelty_func,
+    musa_to_graph
+)
+
 
 warnings.filterwarnings("ignore")
 log.basicConfig(level=log.INFO)
@@ -16,6 +27,34 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 class SymbolicPelt(Segmenter):
     def __init__(self):
         pass
+
+    @staticmethod
+    def feature_extraction(filename: str):
+        musa_object = Musa(filename)
+        g = musa_to_graph(musa_object)
+        mat = nx.attr_matrix(g)[0]
+        n = get_novelty_func(mat)
+        nn = np.reshape(n, (n.size, 1))
+        return nn
+
+    def _get_structure_boundaries(
+        self,
+        filename
+    ):
+
+        musa_object = Musa(filename)
+        signal = self.feature_extraction(filename)
+
+        try:
+            log.info("Fitting to novelty curve")
+            algo = rpt.KernelCPD(kernel="linear").fit(signal)
+            log.info("Predicting result")
+            result = algo.predict(n_bkps=15)
+        except:
+            warnings.warn("No structure found.")
+            result = [0, len(musa_object.notes)-1]
+        return [musa_object.notes[n].start_sec for n in result]
+
 
     def fit(self, filename):
         file = Path(filename)
@@ -34,17 +73,19 @@ class SymbolicPelt(Segmenter):
 
 
 if __name__ == "__main__":
-    # print(SymbolicPelt().predict("/Users/21415968/Desktop/diploma/symbolic-music-structure-analysis/BPS_FH_Dataset/1/1.mid"))
-    # filename_to_absolute_file = make_set_file_to_absolute_path(BPS_absolute_path, "mid")
-    # for filename in filename_to_absolute_file:
-    # name = filename_to_absolute_file[filename]
-    name = "/Users/21415968/Desktop/diploma/symbolic-music-structure-analysis/data/BPS_FH_Dataset/7/7.mid"
-    result = SymbolicPelt().predict(name)
+    pass
 
-    # log.info(f"Working with {name}")
-    # current_prediction_in_secs = SymbolicPelt().predict(name)
-    # print(current_prediction_in_secs)
-    with open(construct_filename_with_your_extension(name, "_symbolic_pred.txt"), 'w') as f:
-        for bound in result:
-            f.write(str(bound) + "\n")
+    # # print(SymbolicPelt().predict("/Users/21415968/Desktop/diploma/symbolic-music-structure-analysis/BPS_FH_Dataset/1/1.mid"))
+    # # filename_to_absolute_file = make_set_file_to_absolute_path(BPS_absolute_path, "mid")
+    # # for filename in filename_to_absolute_file:
+    # # name = filename_to_absolute_file[filename]
+    # name = "/Users/21415968/Desktop/diploma/symbolic-music-structure-analysis/data/BPS_FH_Dataset/7/7.mid"
+    # result = SymbolicPelt().predict(name)
+    #
+    # # log.info(f"Working with {name}")
+    # # current_prediction_in_secs = SymbolicPelt().predict(name)
+    # # print(current_prediction_in_secs)
+    # with open(construct_filename_with_your_extension(name, "_symbolic_pred.txt"), 'w') as f:
+    #     for bound in result:
+    #         f.write(str(bound) + "\n")
 
